e60d30708ba9ba50e421742fec1ac933af2a604a
fithridwinurazizah <115616241+fithridwinurazizah@users.noreply.github.com>
2022-11-27 20:01:53 +0700

    Delete MODUL4 FITHRI directory

--- MODUL4 FITHRI/config/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $email = $_POST['email'];
-    $query = "SELECT * FROM users where email = '$email'";
-    $result = mysqli_query($koneksi, $query);
-    $row = mysqli_fetch_assoc($result);
-}
-?>
